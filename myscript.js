293e6c8e1ca16af4db58d14b257fc04f97a696c7
Haruhiko Matsuda <hmatsuda@jp.real.com>
2014-04-24 21:11:55 +0900

    invoke script onload window

--- myscript.js
+++ myscript.js
@@ -1,26 +1,31 @@
-var target = document.querySelector('body');
-var config = {
-  childList: true,
-  subtree: true
-};
+function observeMergeButton() {
+  var target = document.querySelector('body');
+  var config = {
+    childList: true,
+    subtree: true,
+    characterData: true
+  };
 
-var observer = new MutationObserver(function(mutations, self) {
-  mutations.forEach(function(mutation) {
-    if (mutation.type === 'childList') {
-      var elements = document.querySelectorAll('#js-pull-merging .merge-branch-action');
-      Array.prototype.forEach.call(elements, function(element) {
-        if (document.getElementsByClassName("js-issue-title")[0].textContent.indexOf("WIP") > -1) {
-          if (element.className.indexOf("primary") > -1) {
-            element.className = element.className.replace(/primary/g, 'disabled').replace(/js-details-target/g, '');
+  var observer = new MutationObserver(function(mutations, self) {
+    mutations.forEach(function(mutation) {
+      if (mutation.type === 'childList') {
+        var elements = document.querySelectorAll('.merge-branch-action');
+        Array.prototype.forEach.call(elements, function(element) {
+          if (document.getElementsByClassName("js-issue-title")[0].textContent.indexOf("WIP") > -1) {
+            if (element.className.indexOf("primary") > -1) {
+              element.className = element.className.replace(/primary/g, 'disabled').replace(/js-details-target/g, '');
+            }
+          } else {
+            if (element.className.indexOf("disabled") > -1) {
+              element.className = element.className.replace(/disabled/g, 'primary js-details-target');
+            }
           }
-        } else {
-          if (element.className.indexOf("disabled") > -1) {
-            element.className = element.className.replace(/disabled/g, 'primary js-details-target');
-          }
-        }
-      });
-    }
+        });
+      }
+    });
   });
-});
 
-observer.observe(target, config);
+  observer.observe(target, config);
+}
+
+window.onload = observeMergeButton;
