4ba9e746e6a9cf760bb4ed2661c896840d51d220
Andrew Hills <ahills@salsify.com>
2015-07-16 09:49:40 -0400

    Restore observer

--- myscript.js
+++ myscript.js
@@ -1,6 +1,32 @@
-window.onload = function() {
-  element = document.querySelector(".merge-branch-action");
+function toggleMergeButton(element) {
   if (element.className.indexOf("primary") > -1) {
     element.className = element.className.replace(/primary/g, 'disabled').replace(/js-details-target/g, '');
   }
+}
+
+function observeMergeButton() {
+  var target = document.querySelector('body');
+  var config = {
+    childList: true,
+    subtree: true,
+    characterData: true
+  };
+
+  var observer = new MutationObserver(function(mutations, self) {
+    mutations.forEach(function(mutation) {
+      if (mutation.type === 'characterData' || mutation.type === 'childList') {
+        var elements = document.querySelectorAll('.merge-branch-action');
+        Array.prototype.forEach.call(elements, function(element) {
+          toggleMergeButton(element);
+        });
+      }
+    });
+  });
+
+  observer.observe(target, config);
+}
+
+window.onload = function() {
+  toggleMergeButton(document.querySelector(".merge-branch-action"));
+  observeMergeButton();
 }
